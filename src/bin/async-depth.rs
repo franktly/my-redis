3a51e818909d89f81db034b907845a32fee07130
dreamflytly <dreamflytly@163.com>
2022-04-28 20:17:34 +0800

    update

--- src/bin/async-depth.rs
+++ src/bin/async-depth.rs
@@ -1,13 +1,18 @@
 use tokio::net::TcpStream;
-use std::pin::Pin;
 use std::task::{Context, Poll};
+use std::future::Future;
+use std::pin::Pin;
+use std::time::{Duration, Instant};
 
-
-pub trait Future{
-    type Output;
-
-    fn poll(self:Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output>;
-}
+/*
+ * 
+ * pub trait Future{
+ *     type Output;
+ * 
+ *     fn poll(self:Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output>;
+ * }
+ * 
+ */
 
 async fn my_async_fn()
 {
@@ -16,8 +21,86 @@ async fn my_async_fn()
     println!("async TCP operation complete");
 }
 
-#[tokio::main]
-async fn main() {
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay{
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
+        ->Poll<&'static str>
+    {
+        if Instant::now() >= self.when{
+            println!("Hello World");
+            Poll::Ready("done")
+        }
+        else{
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
+enum MainFuture{
+    // Initialized, never polled
+    State0,
+    // Waiting on `Delay`, i.e. the `future.await`line
+    State1(Delay),
+    // The future has completed
+    Terminated,
+}
+
+impl Future for MainFuture{
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>)
+        -> Poll<()>
+    {
+        use MainFuture::*;
+        loop {
+            match *self {
+                State0 => {
+                    let when = Instant::now() + Duration::from_secs(3);
+                    let fut = Delay{when};
+                    *self = State1(fut); // state switch
+                }
+                State1(ref mut my_future)=> {
+                    match Pin::new(my_future).poll(cx){
+                        Poll::Ready(out) =>
+                        {
+                            *self = Terminated; // state switch 
+                            return Poll::Ready(())
+                        }
+                        Poll::Pending =>{
+                            return Poll::Pending;
+                        }
+                    }
+                }
+                Terminated => {
+                    panic!("future polled after completion!!!")
+                }
+            }
+        }
+    }
+}
+
+async fn async_fn_test()
+{
     let fut = my_async_fn();
     fut.await;
+}
+async fn delay_future_test()
+{
+    let when = Instant::now() + Duration::from_secs(3);
+    let future = Delay{ when };
+    future.await;
+}
+
+#[tokio::main]
+async fn main() {
+
+    async_fn_test().await;
+
+    delay_future_test().await;
 }
